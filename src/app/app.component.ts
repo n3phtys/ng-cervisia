895304e1ac77008fff82bdc23332fe431861c235
Christopher Kaag <christopher.kaag@gmail.com>
2018-06-21 16:03:12 +0200

    hard-reloading whole page every 1-2 days now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewContainerRef } from '@angular/core';
 import { TabService } from './tab.service';
 import { TabActive } from './tab-active.enum';
 import { BackendService } from './backend.service';
+import { min } from 'rxjs/operator/min';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,21 @@ export class AppComponent {
   public tab = TabActive;
 
 
-  constructor(public tabservice: TabService, public backendService: BackendService , vcr: ViewContainerRef) {
+  constructor(public tabservice: TabService, public backendService: BackendService, vcr: ViewContainerRef) {
     this.backendService.toastr.setRootViewContainerRef(vcr);
-      
+
+    this.reloadAfterRandomPeriod();
+
+  }
+
+
+  reloadAfterRandomPeriod() {
+    let hour = 1000 * 3600
+    let minimalMillis = hour * 24;
+    let maximalMillis = hour * 48;
+    let actualMillis = Math.random() * (maximalMillis - minimalMillis) + minimalMillis;
+
+    setTimeout(function () { window.location.reload(true); }, actualMillis);
   }
 }
+
